5d8c4aeceb84aaa2dddaddc3c3f4b686b51f2420
Marcelo Frias <mffrias@gmail.com>
2022-09-12 17:04:41 -0300

    add file

--- tests/forArielGodio/findFirstZero/bug01/FindFirstZero.java
+++ tests/forArielGodio/findFirstZero/bug01/FindFirstZero.java
@@ -1,11 +1,11 @@
-package forArielGodio;
+package forArielGodio.findFirstZero.bug01;
 public class FindFirstZero {
 	 //@ ensures x.length == 0 ==> \result == -1;
    	 //@ ensures 0 <= \result && \result < x.length ==> x[\result] == 0 && (\forall int i; 0 <= i && i < \result; x[i] != 0);
 	 //@ ensures \result == -1 ==> (\forall int i; 0 <= i && i < x.length; x[i] != 0);
     	 public static int findFirstZero(int[] x) {
 //         	assert x.length >= 0;  //NOT IMPLEMENTED
-         	if (x.length == 0) {//if (x.length == 0) {
+         	if (x.length != 0) {//if (x.length == 0) {
             		return -1;
         	} else {
             		int index = 0;

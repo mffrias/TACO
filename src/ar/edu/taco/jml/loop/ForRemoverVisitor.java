e6450f8625199bea3e4508b479b139d9e44f196b
mffrias <mffrias@gmail.com>
2025-10-04 16:53:29 -0600

    Added for supporting for loops

---
+++ src/ar/edu/taco/jml/loop/ForRemoverVisitor.java
@@ -0,0 +1,50 @@
+package ar.edu.taco.jml.loop;
+
+import ar.edu.taco.utils.jml.JmlAstClonerStatementVisitor;
+import org.jmlspecs.checker.JmlLoopStatement;
+import org.multijava.mjc.*;
+import org.multijava.util.compiler.TokenReference;
+
+public class ForRemoverVisitor extends JmlAstClonerStatementVisitor {
+
+
+    public void visitJmlLoopStatement(JmlLoopStatement self) {
+        (self.loopStmt()).accept(this);
+        JStatement unrolledJmlLoop = (JStatement)this.getStack().pop();
+        this.getStack().push(unrolledJmlLoop);
+    }
+
+
+    @Override
+    public void visitForStatement(JForStatement self) {
+        self.body().accept(this);
+        JStatement newBody = (JStatement) this.getStack().pop();
+        // for required statements and expressions
+
+        TokenReference theNewReference = self.getTokenReference();
+        JExpression theNewCondition = self.cond();
+
+        int amountIncrements = ((JExpressionListStatement) self.incr()).getExpressions().length;
+        JStatement[] theNewBodyArray = new JStatement[amountIncrements + 1];
+        theNewBodyArray[0] = newBody;
+        for (int index = 0; index < amountIncrements; index++) {
+            JStatement theStatement = null;
+            Object theFutureStatement = (((JExpressionListStatement) self.incr()).getExpressions())[index];
+            if (theFutureStatement instanceof JExpression) {
+                theStatement = new JExpressionStatement(self.getTokenReference(), (JExpression) theFutureStatement, self.getComments());
+            } else {
+                theStatement = (JStatement) theFutureStatement;
+            }
+            theNewBodyArray[index+1] = theStatement;
+        }
+
+        JStatement theNewBody = new JBlock(theNewReference, theNewBodyArray, self.getComments());
+        JWhileStatement theNewStatementr = new JWhileStatement(theNewReference, theNewCondition, theNewBody, self.getComments());
+        JStatement[] theVarDeclAndWhileArray = new JStatement[2];
+        theVarDeclAndWhileArray[0] = self.init();
+        theVarDeclAndWhileArray[1] = theNewStatementr;
+        JStatement theVarDeclAndTheWhile = new JBlock(theNewReference, theVarDeclAndWhileArray, self.getComments());
+
+        this.getStack().push(theVarDeclAndTheWhile);
+    }
+}

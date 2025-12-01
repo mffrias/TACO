43df4991d0fc188f1e2e325bb3502ac8d59d660e
sofiadeharo <sofiadeharom@gmail.com>
2025-12-01 14:15:40 -0700

    change

--- tests/genai/chatGPT/Select.java
+++ tests/genai/chatGPT/Select.java
@@ -17,7 +17,7 @@ public class Select {
         }
     }
 
-    private static long factorial(int num) {
+    public static long fact(int num) {
         long result = 1;
         for (int i = 2; i <= num; i++) {
             result *= i;
